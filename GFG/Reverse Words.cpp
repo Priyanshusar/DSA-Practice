63bc7a7e6d6159445d759595ba319b8386fddb65
Priyanshu Saraswat <priyanshusaraswat350@gmail.com>
2026-02-10 23:20:02 +0530

    Reverse Words

---
+++ GFG/Reverse Words.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    string reverseWords(string &s) {
+        vector<string> words;
+        string temp = "";
+
+        // Step 1: Extract words
+        for (char c : s) {
+            if (c == '.') {
+                if (!temp.empty()) {
+                    words.push_back(temp);
+                    temp = "";
+                }
+            } else {
+                temp += c;
+            }
+        }
+
+        // Add last word if present
+        if (!temp.empty()) {
+            words.push_back(temp);
+        }
+
+        // Step 2: Reverse words
+        reverse(words.begin(), words.end());
+
+        // Step 3: Join with single dot
+        string result = "";
+        for (int i = 0; i < words.size(); i++) {
+            result += words[i];
+            if (i != words.size() - 1)
+                result += ".";
+        }
+
+        return result;
+    }
+};
