693c32f19c46920b614072ea81ad306ca5649309
Priyanshu Saraswat <priyanshusaraswat350@gmail.com>
2026-02-13 21:57:08 +0530

    Majority Element I

---
+++ Leetcode/Majority_Element_1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+      int count = 0,ele = 0;
+      for(int i=0;i<nums.size();i++)
+      {
+        if(count==0)
+        {
+            count=1;
+            ele = nums[i];
+        }
+        if(ele == nums[i])
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+      } 
+      return ele; 
+    }
+};
