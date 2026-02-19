0f4f15e8e04369f2bfe1a3ae9df0c2be39ab204c
Priyanshu Saraswat <priyanshusaraswat350@gmail.com>
2026-02-19 22:09:09 +0530

    Prime Number

---
+++ GFG/Prime_Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool isPrime(int n) {
+        
+        if(n <= 1) return false;   // 0 and 1 are not prime
+        
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                return false;      // divisor found
+            }
+        }
+        
+        return true;   // no divisor found
+    }
+};
