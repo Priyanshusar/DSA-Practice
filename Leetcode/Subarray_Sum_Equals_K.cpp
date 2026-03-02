81f53ad70c6f4c1b8527b3ed65658ea7f80aa6fc
Priyanshu Saraswat <priyanshusaraswat350@gmail.com>
2026-03-02 21:01:22 +0530

    Subarray With Sum k

---
+++ Leetcode/Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> map; // sum,freq
+
+        int sum = 0;
+        int count = 0;
+        map[0] = 1;
+
+        for (int i=0; i<nums.size(); i++) {
+            sum+=nums[i];
+
+            if (map.count(sum-k)) count+=map[sum-k];
+
+            map[sum]++;
+        }
+        return count;
+    }
+};
