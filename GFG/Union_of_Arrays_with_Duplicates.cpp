740ab7e39de5669dbd465e4efda1f0674d22c6a5
Priyanshu Saraswat <priyanshusaraswat350@gmail.com>
2026-03-08 23:54:53 +0530

    Union of Arrays with Duplicates

---
+++ GFG/Union_of_Arrays_with_Duplicates.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        
+        set<int> s;
+        
+        for(int i = 0; i < a.size(); i++)
+        {
+            s.insert(a[i]);
+        }
+        
+        for(int i = 0; i < b.size(); i++)
+        {
+            s.insert(b[i]);
+        }
+        
+        vector<int> ans(s.begin(), s.end());
+        
+        return ans;
+    }
+};
